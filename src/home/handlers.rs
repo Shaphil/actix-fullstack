b66f0a15b566e80362a1beaadde7d5f4b3c38bba
Md. Abdullah Al Mahmud <shaphil.mahmud@gmail.com>
2024-08-18 21:23:04 +0600

    Return `Json` response from home handlers

--- src/home/handlers.rs
+++ src/home/handlers.rs
@@ -1,11 +1,22 @@
-use actix_web::{get, web, Responder};
+use crate::home::models::HomeResponse;
+
+use actix_web::web::Json;
+use actix_web::{get, web, Error, Responder};
 
 #[get("/hello/{name}")]
-pub async fn greet(name: web::Path<String>) -> impl Responder {
-    format!("Hello {name}!")
+pub async fn greet(name: web::Path<String>) -> Result<impl Responder, Error> {
+    let response = HomeResponse {
+        message: format!("Hello {name}!")
+    };
+
+    Ok(Json(response))
 }
 
 #[get("/test")]
-pub async fn test() -> impl Responder {
-    "Testing...".to_string()
+pub async fn test() -> Result<impl Responder, Error> {
+    let response = HomeResponse {
+        message: "Testing...".to_string()
+    };
+
+    Ok(Json(response))
 }
