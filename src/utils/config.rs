b3cf907f896ff6e6e5d63a644f1248c36e036125
Md. Abdullah Al Mahmud <shaphil.mahmud@gmail.com>
2024-08-18 19:54:48 +0600

    Add application configurations in `config.rs`

---
+++ src/utils/config.rs
@@ -0,0 +1,28 @@
+use std::env;
+use std::env::VarError;
+use lazy_static::lazy_static;
+
+lazy_static! {
+    pub static ref HOST: String = set_host();
+    pub static ref PORT: u16 = set_port();
+}
+
+fn get_env(key: &str) -> Result<String, VarError> {
+    dotenv::dotenv().ok();
+    env::var(key)
+}
+
+fn set_host() -> String {
+    get_env("HOST").unwrap()
+}
+
+fn set_port() -> u16 {
+    get_env("PORT").unwrap().parse::<u16>().unwrap()
+}
+
+pub fn get_address() -> (String, u16) {
+    let host = (*HOST).clone();
+    let port = (*PORT).clone();
+
+    (host, port)
+}
