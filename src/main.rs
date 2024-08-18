26a5934543a678b539a1c37a47d16531fa2d3f80
Md. Abdullah Al Mahmud <shaphil.mahmud@gmail.com>
2024-08-18 19:56:54 +0600

    Set application `host` and `port` from `utils::config::get_address`
    Separate application initialization code into `init()`

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod utils;
 use actix_web::middleware::Logger;
 use actix_web::{get, web, App, HttpServer, Responder};
 use utils::log::set_logger;
+use utils::config::get_address;
 
 
 #[get("/hello/{name}")]
@@ -10,18 +11,23 @@ async fn greet(name: web::Path<String>) -> impl Responder {
     format!("Hello {name}!")
 }
 
-#[actix_web::main] // or #[tokio::main]
-async fn main() -> std::io::Result<()> {
+fn init() {
     set_logger();
     dotenv::dotenv().ok();
     env_logger::init();
+}
+
+#[actix_web::main] // or #[tokio::main]
+async fn main() -> std::io::Result<()> {
+    init();
+    let (host, port) = get_address();
 
     HttpServer::new(|| {
         App::new()
             .wrap(Logger::default())
             .service(greet)
     })
-        .bind(("127.0.0.1", 8080))?
+        .bind((host, port))?
         .run()
         .await
 }
