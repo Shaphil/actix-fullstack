7451055feff4d4edbf1f913992131018b2b93bdf
Md. Abdullah Al Mahmud <shaphil.mahmud@gmail.com>
2024-08-18 23:00:27 +0600

    Add dummy query to test database setup

--- src/home/handlers.rs
+++ src/home/handlers.rs
@@ -1,7 +1,10 @@
-use crate::home::models::HomeResponse;
-
 use actix_web::web::Json;
 use actix_web::{get, web, Error, Responder};
+use sea_orm::DatabaseBackend::Postgres;
+use sea_orm::{ConnectionTrait, Statement};
+
+use crate::home::models::HomeResponse;
+use crate::utils::app_state::AppState;
 
 #[get("/hello/{name}")]
 pub async fn greet(name: web::Path<String>) -> Result<impl Responder, Error> {
@@ -13,7 +16,12 @@ pub async fn greet(name: web::Path<String>) -> Result<impl Responder, Error> {
 }
 
 #[get("/test")]
-pub async fn test() -> Result<impl Responder, Error> {
+pub async fn test(app_state: web::Data<AppState>) -> Result<impl Responder, Error> {
+    // db test
+    let _res = app_state.db
+        .query_all(Statement::from_string(Postgres, "select * from user;"))
+        .await.unwrap();
+
     let response = HomeResponse {
         message: "Testing...".to_string()
     };
