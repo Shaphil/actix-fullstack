5a3f346de677d872e8e6fb23fdca7d7d2cdb29b4
Md. Abdullah Al Mahmud <shaphil.mahmud@gmail.com>
2024-08-25 22:01:37 +0600

    Parse `DateTime` object from datetime `String`
    * Expect date/time to be in the format defined by `date_format`

--- src/home/handlers.rs
+++ src/home/handlers.rs
@@ -1,10 +1,11 @@
-use crate::home::models::{HomeResponse, User};
+use crate::home::models::{HomeResponse, UserRequest};
 use crate::utils::app_state::AppState;
 use actix_web::web::Json;
 use actix_web::{get, post, web, Error, HttpResponse, Responder};
 use entity::user::{ActiveModel, Model};
 use sea_orm::DatabaseBackend::Postgres;
 use sea_orm::{ActiveModelTrait, ActiveValue, ConnectionTrait, Statement};
+use sea_orm::prelude::DateTime;
 
 #[get("/hello/{name}")]
 pub async fn greet(name: web::Path<String>) -> Result<impl Responder, Error> {
@@ -30,15 +31,23 @@ pub async fn test(app_state: web::Data<AppState>) -> Result<impl Responder, Erro
 }
 
 #[post("/create-user")]
-pub async fn create_user(payload: Json<User>, app_state: web::Data<AppState>) -> Result<impl Responder, Error> {
+pub async fn create_user(payload: Json<UserRequest>, app_state: web::Data<AppState>) -> Result<impl Responder, Error> {
+    let date_format = "%Y-%m-%d %H:%M:%S";
+
+    let last_login_str = payload.last_login.as_str();
+    let last_login = DateTime::parse_from_str(last_login_str, date_format).unwrap();
+
+    let date_joined_str = payload.date_joined.as_str();
+    let date_joined = DateTime::parse_from_str(date_joined_str, date_format).unwrap();
+
     let user = ActiveModel {
         user_name: ActiveValue::Set(payload.user_name.clone()),
         first_name: ActiveValue::Set(Option::from(payload.first_name.clone())),
         last_name: ActiveValue::Set(Option::from(payload.last_name.clone())),
         email: ActiveValue::Set(payload.email.clone()),
         is_active: ActiveValue::Set(payload.is_active.clone()),
-        last_login: ActiveValue::Set(payload.last_login.clone()),
-        date_joined: ActiveValue::Set(payload.date_joined.clone()),
+        last_login: ActiveValue::Set(Option::from(last_login)),
+        date_joined: ActiveValue::Set(Option::from(date_joined)),
         password: ActiveValue::Set(payload.password.clone()),
         ..Default::default()
     };
