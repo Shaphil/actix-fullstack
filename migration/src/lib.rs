d1dc87a48a37aadd1cb0fc63ec4668f8a5dc9979
Md. Abdullah Al Mahmud <shaphil.mahmud@gmail.com>
2024-08-25 14:20:49 +0600

    Add migration for `User.password` field

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -1,6 +1,8 @@
 pub use sea_orm_migration::prelude::*;
 
 mod m20240818_160752_create_user_table;
+mod m20240822_195452_alter_user_table_add_password_field;
+
 
 pub struct Migrator;
 
@@ -9,6 +11,7 @@ impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
         vec![
             Box::new(m20240818_160752_create_user_table::Migration),
+            Box::new(m20240822_195452_alter_user_table_add_password_field::Migration)
         ]
     }
 }
---
+++ migration/src/m20240822_195452_alter_user_table_add_password_field.rs
@@ -0,0 +1,33 @@
+use sea_orm_migration::prelude::*;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .alter_table(Table::alter()
+                .table(User::Table)
+                .add_column_if_not_exists(
+                    ColumnDef::new(User::Password)
+                        .string()
+                        .not_null()
+                        .default(String::new())
+                ).to_owned()
+            )
+            .await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .drop_table(Table::drop().table(User::Table).to_owned())
+            .await
+    }
+}
+
+#[derive(DeriveIden)]
+enum User {
+    Table,
+    Password,
+}
