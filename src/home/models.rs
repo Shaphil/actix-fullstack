09e813b9fa57ac2c0c48e93d7044852eaadf47d9
Md. Abdullah Al Mahmud <shaphil.mahmud@gmail.com>
2024-08-25 14:31:38 +0600

    Add response model `User` to `models`

--- src/home/models.rs
+++ src/home/models.rs
@@ -1,6 +1,20 @@
-use serde::Serialize;
+use sea_orm::prelude::DateTime;
+use serde::{Deserialize, Serialize};
 
 #[derive(Serialize)]
 pub struct HomeResponse {
     pub message: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct User {
+    pub id: i32,
+    pub user_name: String,
+    pub first_name: Option<String>,
+    pub last_name: Option<String>,
+    pub email: String,
+    pub is_active: Option<bool>,
+    pub last_login: Option<DateTime>,
+    pub date_joined: Option<DateTime>,
+    pub password: String,
 }
