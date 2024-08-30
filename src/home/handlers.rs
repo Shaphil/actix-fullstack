5dc726622acdd5c1d46390ef38cfe65c31eb9a7f
Md. Abdullah Al Mahmud <shaphil.mahmud@gmail.com>
2024-08-30 21:08:24 +0600

    Set default values for fields `last_login` and `date_joined`
    * Set `last_login` to `None`
    * Set `date_joined` to `Utc::now()`

--- src/home/handlers.rs
+++ src/home/handlers.rs
@@ -5,7 +5,7 @@ use actix_web::{get, post, web, Error, HttpResponse, Responder};
 use entity::user::{ActiveModel, Model};
 use sea_orm::DatabaseBackend::Postgres;
 use sea_orm::{ActiveModelTrait, ActiveValue, ConnectionTrait, Statement};
-use sea_orm::prelude::DateTime;
+use chrono::Utc;
 
 #[get("/hello/{name}")]
 pub async fn greet(name: web::Path<String>) -> Result<impl Responder, Error> {
@@ -32,22 +32,14 @@ pub async fn test(app_state: web::Data<AppState>) -> Result<impl Responder, Erro
 
 #[post("/create-user")]
 pub async fn create_user(payload: Json<UserRequest>, app_state: web::Data<AppState>) -> Result<impl Responder, Error> {
-    let date_format = "%Y-%m-%d %H:%M:%S";
-
-    let last_login_str = payload.last_login.as_str();
-    let last_login = DateTime::parse_from_str(last_login_str, date_format).unwrap();
-
-    let date_joined_str = payload.date_joined.as_str();
-    let date_joined = DateTime::parse_from_str(date_joined_str, date_format).unwrap();
-
     let user = ActiveModel {
         user_name: ActiveValue::Set(payload.user_name.clone()),
         first_name: ActiveValue::Set(Option::from(payload.first_name.clone())),
         last_name: ActiveValue::Set(Option::from(payload.last_name.clone())),
         email: ActiveValue::Set(payload.email.clone()),
         is_active: ActiveValue::Set(payload.is_active.clone()),
-        last_login: ActiveValue::Set(Option::from(last_login)),
-        date_joined: ActiveValue::Set(Option::from(date_joined)),
+        last_login: ActiveValue::Set(None),
+        date_joined: ActiveValue::Set(Option::from(Utc::now().naive_utc())),
         password: ActiveValue::Set(payload.password.clone()),
         ..Default::default()
     };
