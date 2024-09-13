ad923bce2a273f0c00598b3cffe58c9972ecac6e
Md. Abdullah Al Mahmud <shaphil.mahmud@gmail.com>
2024-09-14 01:31:09 +0600

    Setup Migration with Sea-ORM
    * Add `migration` crate
      - Run command `sea-orm-cli migrate init`
    * Add `migration` as a workspace member and dependency in root `Cargo.toml`
    * Add migration to `main`
      - Get database connection
      - Run migration on application startup on this connection
      - Add database connection to `app_data` through `AppState` on startup

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 mod utils;
 mod home;
 
+use crate::utils::app_state::AppState;
 use actix_web::middleware::Logger;
-use actix_web::{App, HttpServer};
-use utils::config::get_address;
+use actix_web::{web, App, HttpServer};
+use migration::{Migrator, MigratorTrait};
+use utils::config::{get_address, get_db_connection};
 use utils::log::set_logger;
 
 fn init() {
@@ -14,11 +16,16 @@ fn init() {
 #[actix_web::main] // or #[tokio::main]
 async fn main() -> std::io::Result<()> {
     init();
+
+    let db = get_db_connection().await;
+    Migrator::up(&db, None).await.unwrap();
+
     let (host, port) = get_address();
     println!("Server running at http://{}:{}", host, port);
 
-    HttpServer::new(|| {
+    HttpServer::new(move || {
         App::new()
+            .app_data(web::Data::new(AppState { db: db.clone() }))
             .wrap(Logger::default())
             .configure(home::urls::routes)
     })
