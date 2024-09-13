2a73ce3e47ad513ec54561e2df8c7fc0950a71ca
Md. Abdullah Al Mahmud <shaphil.mahmud@gmail.com>
2024-09-14 01:22:11 +0600

    Add `DATABASE_URL` to `config`
    * Add function `set_db` to set `DATABASE_URL` from environment variable
    * Add function `get_db_connection`
      - Set suggested database connection options
      - Return database connection

--- src/utils/config.rs
+++ src/utils/config.rs
@@ -1,10 +1,13 @@
 use std::env;
 use std::env::VarError;
+use std::time::Duration;
 use lazy_static::lazy_static;
+use sea_orm::{ConnectOptions, Database, DatabaseConnection};
 
 lazy_static! {
     pub static ref HOST: String = set_host();
     pub static ref PORT: u16 = set_port();
+    pub static ref DATABASE_URL: String = set_db();
 }
 
 fn get_env(key: &str) -> Result<String, VarError> {
@@ -25,4 +28,21 @@ pub fn get_address() -> (String, u16) {
     let port = (*PORT).clone();
 
     (host, port)
+}
+
+fn set_db() -> String {
+    get_env("DATABASE_URL").unwrap()
+}
+
+pub async fn get_db_connection() -> DatabaseConnection {
+    let mut opt = ConnectOptions::new(DATABASE_URL.clone());
+    opt.max_connections(100)
+        .min_connections(5)
+        .connect_timeout(Duration::from_secs(8))
+        .acquire_timeout(Duration::from_secs(8))
+        .max_lifetime(Duration::from_secs(8))
+        .sqlx_logging(true)
+        .sqlx_logging_level(log::LevelFilter::Info);
+
+    Database::connect(opt).await.unwrap()
 }
