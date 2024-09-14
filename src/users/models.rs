2d19d894458e4dcacb21a8d6c7d14a9ecc95985e
Md. Abdullah Al Mahmud <shaphil.mahmud@gmail.com>
2024-09-15 03:18:59 +0600

    Add struct `ApiResponse` to `users::models`

--- src/users/models.rs
+++ src/users/models.rs
@@ -1,5 +1,11 @@
 use serde::{Deserialize, Serialize};
 
+
+#[derive(Serialize)]
+pub struct ApiResponse {
+    pub message: String,
+}
+
 #[derive(Serialize, Deserialize, Debug)]
 pub struct UserRequest {
     pub username: String,
