25caa2b8a7869f99940e16bd329ca1703d128bc3
Md. Abdullah Al Mahmud <shaphil.mahmud@gmail.com>
2024-09-15 03:20:10 +0600

    Add error handling to `create_user` endpoint
    * Add TODOs

--- src/users/handlers.rs
+++ src/users/handlers.rs
@@ -1,9 +1,9 @@
-use actix_web::{post, web, Error, HttpResponse, Responder};
 use actix_web::web::Json;
+use actix_web::{post, web, Error, HttpResponse, Responder};
+use entity::user::ActiveModel;
 use sea_orm::{ActiveModelTrait, ActiveValue};
-use entity::user::{ActiveModel, Model};
 
-use crate::users::models::UserRequest;
+use crate::users::models::{ApiResponse, UserRequest};
 use crate::utils::app_state::AppState;
 
 
@@ -16,6 +16,9 @@ pub async fn create_user(payload: Json<UserRequest>, app_state: web::Data<AppSta
         email: ActiveValue::Set(String::from(payload.email.clone())),
         password: ActiveValue::Set(payload.password.clone()),
         is_active: ActiveValue::Set(Option::from(payload.is_active.clone())),
+        // TODO: create `serializer.rs` for this
+        // TODO: and use `https://github.com/waltzofpearls/dateparser/tree/main/dateparser`
+        // TODO: to parse the dates
         last_login: ActiveValue::Set(None),
         date_joined: ActiveValue::Set(None),
         created_at: ActiveValue::Set(None),
@@ -23,6 +26,12 @@ pub async fn create_user(payload: Json<UserRequest>, app_state: web::Data<AppSta
         ..Default::default()
     };
 
-    let user: Model = user.insert(&app_state.db).await.unwrap();
-    Ok(HttpResponse::Ok().json(user))
+    let result = user.insert(&app_state.db).await;
+    match result {
+        Ok(user) => Ok(HttpResponse::Ok().json(user)),
+        Err(err) => {
+            let response = ApiResponse { message: err.to_string() };
+            Ok(HttpResponse::BadRequest().json(response))
+        }
+    }
 }
