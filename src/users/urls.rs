6bf78b2612a6c549158f663e539cef045f9353ff
Md. Abdullah Al Mahmud <shaphil.mahmud@gmail.com>
2024-10-16 02:41:39 +0600

    Protect CRUD APIs with `authenticate` middleware

--- src/users/urls.rs
+++ src/users/urls.rs
@@ -1,15 +1,22 @@
+use actix_web::middleware::from_fn;
 use actix_web::web;
 use crate::users::handlers;
+use crate::users::middlewares::authenticate;
 
 pub fn routes(config: &mut web::ServiceConfig) {
-    config.service(
-        web::scope("/users")
-            .service(handlers::create_user)
-            .service(handlers::get_users)
-            .service(handlers::get_user)
-            .service(handlers::update_user)
-            .service(handlers::update_user_full)
-            .service(handlers::delete_user)
-            .service(handlers::login)
-    );
+    config
+        .service(
+            web::scope("/auth/users")
+                .wrap(from_fn(authenticate))
+                .service(handlers::create_user)
+                .service(handlers::update_user)
+                .service(handlers::update_user_full)
+                .service(handlers::delete_user)
+        )
+        .service(
+            web::scope("/users")
+                .service(handlers::get_users)
+                .service(handlers::get_user)
+                .service(handlers::login)
+        );
 }
